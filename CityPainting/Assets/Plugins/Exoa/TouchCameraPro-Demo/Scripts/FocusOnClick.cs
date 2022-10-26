7d64afb46e638c2bedb3b267facb58e59c83aff9
Cihan-Desktop <cihanozgur1988@gmail.com>
2022-10-26 13:04:20 +0300

    no message

---
+++ CityPainting/Assets/Plugins/Exoa/TouchCameraPro-Demo/Scripts/FocusOnClick.cs
@@ -0,0 +1,23 @@
+﻿using Exoa.Events;
+using Lean.Common;
+
+public class FocusOnClick : LeanSelectableBehaviour
+{
+    public bool follow;
+    public bool focusOnFollow;
+
+    protected override void OnSelected(LeanSelect select)
+    {
+        print("OnSelect " + gameObject.name);
+        if (follow)
+            CameraEvents.OnRequestObjectFollow?.Invoke(gameObject, focusOnFollow);
+        else
+            CameraEvents.OnRequestObjectFocus?.Invoke(gameObject);
+    }
+    protected override void OnDeselected(LeanSelect select)
+    {
+        print("OnDeselect " + gameObject.name);
+        if (follow)
+            CameraEvents.OnRequestObjectFollow?.Invoke(null, focusOnFollow);
+    }
+}
