7b474dfe901c3d6b2db124202fa661af28367efb
Cihan-Desktop <cihanozgur1988@gmail.com>
2022-10-31 13:23:02 +0300

    no message

---
+++ CityPainting/Assets/_GameAssets/_Scripts/M_AppIcon.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class M_AppIcon : MonoBehaviour
+{
+
+    private void Start()
+    {
+        AppIconChanger.iOS.SetAlternateIconName("icon_" + Random.Range(0, 4).ToString("00"));
+
+    }
+
+
+    private void Awake()
+    {
+        II = this;
+    }
+    public static M_AppIcon II;
+
+    public static M_AppIcon I
+    {
+        get
+        {
+            if (II == null)
+            {
+                II = GameObject.Find("M_AppIcon").GetComponent<M_AppIcon>();
+            }
+
+            return II;
+        }
+    }
+}
